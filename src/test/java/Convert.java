e3760ee6d857ce65e6c19a87aae1710f6e534f98
paoloearth <paoloearth@gmail.com>
2021-01-07 13:19:14 +0100

    Simplify .gitignore

--- src/test/java/Convert.java
+++ src/test/java/Convert.java
@@ -2,9 +2,6 @@
 
 
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -12,21 +9,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Convert {
 
 
-    @ParameterizedTest
-    @CsvSource({"1, Dispari", "17, Dispari", "99, Dispari"})
-    void ControllaDispari(int number, String expected)
-    {
-        Cipolla F = new Cipolla();
-        assertEquals(expected, F.convert(number));
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = {4,6,24})
-    void ControllaPari(int number)
-    {
-        Cipolla F = new Cipolla();
-        assertEquals("Pari", F.convert(number));
-    }
 
 
 
