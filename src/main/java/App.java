a82bb2c43ae331196a63d6def49f50d7a16be667
paoloearth <paoloearth@gmail.com>
2021-01-08 18:22:31 +0100

    Fixing last things

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,5 @@
 import javafx.application.Application;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
