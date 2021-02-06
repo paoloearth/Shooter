6b4416b50b6550e84f23dfaa3e48d1230584f764
paoloearth <paoloearth@gmail.com>
2021-02-06 10:14:00 +0100

    Eliminated uneseful parts of code

--- src/main/java/units/shooter_developers/Simulation.java
+++ src/main/java/units/shooter_developers/Simulation.java
@@ -32,55 +32,28 @@ public class Simulation extends Application{
     }
 
     public void start(Stage game_stage) throws Exception {
-        game_stage.setTitle("Call of Pouty: Tergeste Warfare");
+        game_stage.setTitle("Shooter 2D GAME");
 
         Group root = new Group();
         Scene scene = new Scene(root);
         game_stage.setScene(scene);
 
-        Canvas canvas = new Canvas(_width, _height);
-        root.getChildren().add(canvas);
 
-        GraphicsContext gc = canvas.getGraphicsContext2D();
 
-        Image default_background = new Image("default_bg.jpg", _width, _height, false, false);
-
-        ArrayList<String> input_keys = new ArrayList<String>();
 
         // Put here the loop for pressed keys
 
         // Put here the loop for pressed keys
 
-        //get the pressed keys and add to the list
-        scene.setOnKeyPressed(
-                new EventHandler<KeyEvent>()
-                {
-                    public void handle(KeyEvent e)
-                    {
-                        String code = e.getCode().toString();
 
-                        // only add once... prevent duplicates
-                        if ( !input_keys.contains(code) )
-                            input_keys.add( code );
-                    }
-                });
-
-        //get the unpressed keys and remove to the list
-        scene.setOnKeyReleased(
-                new EventHandler<KeyEvent>()
-                {
-                    public void handle(KeyEvent e)
-                    {
-                        String code = e.getCode().toString();
-                        input_keys.remove( code );
-                    }
-                });
 
         final long[] lastNanoTime = {System.nanoTime()};
 
         //ciclo del render della mappa
 
         var reference_to_game_instance = this;
+
+
         new AnimationTimer()
         {
             public void handle(long currentNanoTime)
@@ -89,11 +62,6 @@ public class Simulation extends Application{
                 double t = (currentNanoTime - lastNanoTime[0]) / 1000000000.0;
                 lastNanoTime[0] = currentNanoTime;
 
-                // game logic
-                if (input_keys.contains("LEFT")) {
-                    //implement some response here!
-                }
-                if (input_keys.contains("ESCAPE")) {
                     try {
                         GameMenu pause_menu = new GameMenu(reference_to_game_instance);
                         Stage secondary_stage = new Stage();
@@ -104,21 +72,16 @@ public class Simulation extends Application{
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
+
+                if(_close) {
+                    game_stage.close();
                 }
+            }
 
                 //update objects here!
 
                 //check collisions and illegal movements here!
 
-                if(_close) {
-                    game_stage.close();
-                    return;
-                }
-
-                while(_pause){}
-
-                gc.drawImage(default_background, 0, 0);
-            }
         }.start();
 
         game_stage.show();
