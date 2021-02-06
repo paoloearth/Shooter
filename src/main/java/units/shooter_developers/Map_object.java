b964dbb0c8d9d713794c84d9a792ead524a1f4c0
paoloearth <paoloearth@gmail.com>
2021-02-06 17:03:29 +0100

    Started to modify mapObject

--- src/main/java/units/shooter_developers/Map_object.java
+++ src/main/java/units/shooter_developers/Map_object.java
@@ -1,6 +1,10 @@
 package units.shooter_developers;
 
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import javafx.scene.shape.Rectangle;
 import javafx.util.Pair;
 
@@ -14,20 +18,71 @@ interface Map_object_dynamic{
     public Pair<Integer, Integer> getCoordinates();
 }
 
-public class Map_object {
-    private Pair<Integer, Integer> _coordinates;
-    private ImageView _sprite;
+public abstract class Map_object extends Pane {
+
+    // Contains the PATH to the Image
+    protected String _url;
+
+    // Will store reference file for the image to render
+    protected Image _picture;
+
+    // Will store the current view
+    protected ImageView _view;
+
+    // Will store the map-object's WIDTH & HEIGHT
+    protected  int _row_sheet;
+    protected  int _columns_sheet;
+    protected double _scale;
+    protected int _width;
+    protected int _height;
+
+    // Will store the coordinates
+    protected Pair<Integer, Integer> _coordinates;
+
+    // Will store the type of the object
+    protected String type;
+
+    // Boolean dead;
+    BooleanProperty dead = new SimpleBooleanProperty(false);
+
     private Rectangle _hitbox;
-    private int _width;
-    private int _height;
+
 
     /********************************************************************************/
     /* CONSTRUCTORS                                                                 */
     /********************************************************************************/
 
+    public Map_object(String URL, double SCALE, int ROW_SHEET, 
+                     int COLUMNS_SHEET, int WIDTH, int HEIGHT, 
+                     Pair<Integer, Integer> COORDINATES)
+    {
+        this._height=HEIGHT;
+        this._width = WIDTH;
+        
+        this._scale = SCALE;
+
+        /* Read the picture*/
+        this._picture = retrieve_image(URL, SCALE, HEIGHT ,WIDTH);
+
+        /* Setting the parameters of the image */
+        this._row_sheet = ROW_SHEET;
+        this._columns_sheet = COLUMNS_SHEET;
+
+        /* Setting the initial coordinates of the mapobject */
+        set_coordinates(COORDINATES);
+
+        this._view = new ImageView(this._picture);
+    }
+
+    protected  void set_coordinates(Pair<Integer, Integer> COORDINATES)
+    {
+        this.setLayoutX(COORDINATES.getKey());
+        this.setLayoutY(COORDINATES.getValue());
+    };
+
     public Map_object(){
         this.setCoordinates(0, 0);
-        this._sprite = null;
+        this._view = null;
         this._hitbox = null;
         this._width = 800;
         this._width = 600;
@@ -39,13 +94,14 @@ public class Map_object {
         this._height = height;
     }
 
-    public Map_object(Map_object map_object){
-        _coordinates = map_object._coordinates;
-        _sprite = map_object._sprite;
-        _hitbox = map_object._hitbox;
-        _width = map_object._width;
-        _height = map_object._height;
+    /********************************************************************************/
+    /* FUNCTIONS                                                             */
+    /********************************************************************************/
+    Image retrieve_image(String URL, double SCALE, int HEIGHT, int WIDTH)
+    {
+        return new Image(URL,  WIDTH*SCALE,  HEIGHT*SCALE,  true,  false);
     }
+
 
     /********************************************************************************/
     /* SET/GET METHODS                                                              */
@@ -54,7 +110,7 @@ public class Map_object {
         this._width = map_object._width;
         this._height = map_object._height;
         this._hitbox = map_object._hitbox;
-        this._sprite = map_object._sprite;
+        this._view = map_object._view;
         this._coordinates = map_object._coordinates;
     }
 
@@ -92,11 +148,5 @@ public class Map_object {
         return this._hitbox;
     }
 
-    public int getWidth(){
-        return _width;
-    }
 
-    public int getHeight(){
-        return _height;
-    }
 }
