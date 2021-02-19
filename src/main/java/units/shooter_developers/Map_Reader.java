9ddf71f2bb9c265da5cdbe4823459093bc02a61d
matildecastelli <matilde.castelli17@gmail.com>
2021-02-19 18:50:16 +0100

    Created class and added template for csv files

---
+++ src/main/java/units/shooter_developers/Map_Reader.java
@@ -0,0 +1,12 @@
+package units.shooter_developers;
+
+
+/* TEMPLATE
+       0. URL_spritesheet_mappa
+       1. n_cols, n_rows, cell_size
+       2. set_of_passable_blocks
+       3. set_of_non_passable_block_for_projectiles
+       4. sprite_1_blocco_x, sprite_1_blocco_y,sprite_2_blocco_x, sprite_2_blocco_y
+*/
+public class Map_Reader {
+}
