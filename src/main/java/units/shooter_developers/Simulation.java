38408e175097260cf57590a3efd157f3c4ebabca
paoloearth <paoloearth@gmail.com>
2021-02-19 10:47:55 +0100

    Eliminated unnecessary files

--- src/main/java/units/shooter_developers/Simulation.java
+++ src/main/java/units/shooter_developers/Simulation.java
@@ -1,37 +1,19 @@
 package units.shooter_developers;
 
-import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.event.EventHandler;
-import javafx.geometry.Rectangle2D;
-import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.input.KeyEvent;
+import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
-import javafx.scene.canvas.*;
-import javafx.scene.image.Image;
-import javafx.util.Pair;
 
-import java.util.ArrayList;
 
 public class Simulation extends Application{
 
 
     /* Main component on which we add elements */
-    private final Pane root = new Pane();
-
-    /* Size of the windows */
-    int HEIGHT;
-    int WIDTH;
-
-    /* Status of the game */
-    private boolean _close,_pause;
-
-    Pair<Double,Double> scaling_factors;
-
+     final Pane _root = new Pane();
+     Stage _stage = new Stage();
+     Scene _scene;
 
 
     public static void main(String[] args)
@@ -39,15 +21,12 @@ public class Simulation extends Application{
         launch(args);
     }
 
-    public void start(Stage stage){
+    @Override
+    public void start(Stage stage) throws Exception{
 
-        /* Add windows title */
         stage.setTitle("Shooter");
-
-
-
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
+        Pane R = new Pane();
+        R.getChildren().add(new Button());
 
 
         stage.show();
@@ -55,7 +34,9 @@ public class Simulation extends Application{
 
 
 
+
     public void stop(){
-        this._close = true;
+      boolean _close = true;
     }
+
 }
--- src/test/java/units/shooter_developers/Simulation_Tests.java
+++ src/test/java/units/shooter_developers/Simulation_Tests.java
@@ -1,5 +1,28 @@
 package units.shooter_developers;
 
-public class Simulation_Tests {
+
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
+
+
+
+class Simulation_Test  extends ApplicationTest {
+    @Override
+    public void start(Stage stage) {
+        Scene scene = new Scene(new Pane(), 800, 600);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @Test
+    public void test_set_title() {
+
+
+    }
+
+
 
 }
