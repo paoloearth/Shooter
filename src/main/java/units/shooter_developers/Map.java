2ad075773f3c9a275d2f40b28721dee05349759e
paoloearth <paoloearth@gmail.com>
2021-02-20 09:32:46 +0100

    Declared variables of MAP

--- src/main/java/units/shooter_developers/Map.java
+++ src/main/java/units/shooter_developers/Map.java
@@ -1,4 +1,32 @@
 package units.shooter_developers;
 
+
+import javafx.scene.layout.Pane;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Map {
+
+    /* List of tiles that will be used to fast access the blocks property by indices*/
+    private List<Tile> _tiles = new ArrayList<>();
+
+    /* List of passable blocks */
+    List<Tile> _passable_tiles;
+
+    /* JavaFX component to store and shows the tiles*/
+    Pane _cells = new Pane();
+
+    /* Width & Height of the tiles */
+    private  int _width;
+    private  int _height;
+
+    /* Map_Reader object, delegated to interact with the files */
+    Map_Reader _MR;
+
+    /* URL to the map.csv */
+    String _map_URL;
+
+
+
 }
