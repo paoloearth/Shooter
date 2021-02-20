8b493bc36b2e1335513d805b39da70bad1711978
matildecastelli <matilde.castelli17@gmail.com>
2021-02-20 09:50:57 +0100

    Created function get_positions in order to retrieve the Pair of positions on the map of the player and the teleports

--- src/main/java/units/shooter_developers/Map_Reader.java
+++ src/main/java/units/shooter_developers/Map_Reader.java
@@ -23,9 +23,11 @@ public class Map_Reader {
     Image _tileset;
     private final List<String[]> _lines;
     Integer _cell_side;
-    Pair<Integer,Integer> _num_tiles;         //number of tiles of the map
-    final Set<Integer> _set_of_passable;      //set of passable tiles for the Sprites
-
+    Pair<Integer,Integer> _num_tiles;                              //number of tiles of the map
+    final Set<Integer> _set_of_passable;                           //set of passable tiles for the Player
+    final Set<Integer> _set_of_NOT_passable_for_projectile;
+    Pair<Pair<Integer,Integer>,Pair<Integer,Integer>> _players_positions;  //position of the player in the corresponding map
+    Pair<Pair<Integer,Integer>,Pair<Integer,Integer>> _teleport_positions; //positon of teleports in the corresponding map
     // Constructor
     Map_Reader(String URL) throws IOException {
 
@@ -34,17 +36,13 @@ public class Map_Reader {
         _cell_side = get_cell_side();
         _num_tiles = get_num_of_tiles();
         _set_of_passable = get_tiles_at_row_index(2);
+        _set_of_NOT_passable_for_projectile = get_tiles_at_row_index(3);
+        _players_positions = get_positions(4);
+        _teleport_positions = get_positions(5);
+
 
         //Lines representing the map
         //_map = _lines.stream().skip(Custom_Settings.NUMBER_OF_METADATA_LINES).collect(Collectors.toList());
-    }
-
-    private Set<Integer> get_tiles_at_row_index(int index) {
-        return  Arrays.stream(_lines.get(index)).parallel().mapToInt(Integer::parseInt).boxed().collect(Collectors.toSet());
-    }
-
-    private Pair<Integer, Integer> get_num_of_tiles() {
-        return new Pair<>(Integer.parseInt(_lines.get(1)[0]),Integer.parseInt(_lines.get(1)[1]));
     }
 
     private Image get_tileset() {
@@ -63,6 +61,27 @@ public class Map_Reader {
         return  Integer.parseInt(_lines.get(1)[2]);
     }
 
+    private Pair<Integer, Integer> get_num_of_tiles() {
+        return new Pair<>(Integer.parseInt(_lines.get(1)[0]),Integer.parseInt(_lines.get(1)[1]));
+    }
+
+    /* Reads a set of tiles at a specific row:
+     * - in the 2nd row there is the set of passable tiles for the Sprites
+     * - in the 3rd row there is the set of tiles not passable for the projectiles
+     * */
+    private Set<Integer> get_tiles_at_row_index(int index) {
+        return  Arrays.stream(_lines.get(index)).parallel().mapToInt(Integer::parseInt).boxed().collect(Collectors.toSet());
+    }
+
+    /*
+    Reads the pair of positions of:
+     - the players at row 4th
+     - teleports at row 5th
+      */
+    private Pair<Pair<Integer, Integer>, Pair<Integer, Integer>> get_positions(int index) {
+        var values = Arrays.stream(_lines.get(index)).parallel().mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
+        return new Pair<>(new Pair<>(values.get(0), values.get(1)), new Pair<>(values.get(2), values.get(3)));
+    }
 
 
 
