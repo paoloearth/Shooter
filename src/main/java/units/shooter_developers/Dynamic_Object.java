2b43baf67944b180560a88e61f86aa4501481e03
matildecastelli <matilde.castelli17@gmail.com>
2021-02-20 18:44:40 +0100

    Added constructors

--- src/main/java/units/shooter_developers/Dynamic_Object.java
+++ src/main/java/units/shooter_developers/Dynamic_Object.java
@@ -3,10 +3,23 @@ import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.util.Pair;
 
-public class Dynamic_Object {
+public abstract class Dynamic_Object extends Pictured_Object {
     ObjectProperty<Direction> _current_direction = new SimpleObjectProperty<>();
 
     int _speed;
     int _deltaX, _deltaY;
+
+    //Custom Constructor
+    public Dynamic_Object(Pair<Double,Double> scaling_factors, String url)
+    {
+        super(scaling_factors, url);
+
+    }
+
+    //Custom Constructor
+    public Dynamic_Object( Pair<Double,Double> scaling_factors, String url, int _n_rows, int _n_cols)
+    {
+        super(scaling_factors, url, _n_rows, _n_cols);
+    }
 
 }
