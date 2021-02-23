c83aa7cbbf1061b4cb42de6fbbf9375c425b0d83
matildecastelli <matilde.castelli17@gmail.com>
2021-02-23 17:40:10 +0100

    Create function that locate bonus randomly on the map

--- src/main/java/units/shooter_developers/Bonus_Generator.java
+++ src/main/java/units/shooter_developers/Bonus_Generator.java
@@ -9,9 +9,35 @@ import javafx.scene.layout.Pane;
 import javafx.util.Duration;
 import javafx.util.Pair;
 
-public class Bonus_Generator {
+public class Bonus_Generator extends Pictured_Object{
     BooleanProperty is_waiting = new SimpleBooleanProperty(true);
     Object_Animation anim;
-    boolean already_called = false;
+
+    public Bonus_generator(Pane root, Map M, String url, int _n_rows, int _n_cols, Pair<Double, Double> scaling_factor)
+    {
+        super(scaling_factor,url,_n_rows,_n_cols);
+
+        this._type = "BONUS";
+        this._scale = Custom_Settings.HEART_SCALE;
+
+        update_view();
+
+        generate(M);
+
+
+        // ADD to the main pane
+        root.getChildren().add(this);
+
+    }
+
+    public void generate(Map M)
+    {
+
+        move_to(M.get_random_location());
+        push_inside_border(M);
+        this.getChildren().add(_view);
+
+    }
+
 
 }
