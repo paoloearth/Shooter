567adb4d7bfeea4eba893819e8ed19f587ac0a5c
matildecastelli <matilde.castelli17@gmail.com>
2021-02-23 11:27:02 +0100

    Create properties of class teleport

--- src/main/java/units/shooter_developers/Teleport.java
+++ src/main/java/units/shooter_developers/Teleport.java
@@ -1,5 +1,12 @@
 package units.shooter_developers;
 
+import javafx.animation.RotateTransition;
+import javafx.util.Pair;
+
 public class Teleport extends Pictured_Object{
+
+    RotateTransition rotation;
+    Pair<Integer, Integer> destination;
+
 
 }
