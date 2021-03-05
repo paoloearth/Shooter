f45512070cd7b97ed5df9edf238ea90a43d36f48
matildecastelli <matilde.castelli17@gmail.com>
2021-03-05 20:17:20 +0100

    Map_Reader refactor

--- src/main/java/units/shooter_developers/Map_Reader.java
+++ src/main/java/units/shooter_developers/Map_Reader.java
@@ -22,21 +22,15 @@ public class Map_Reader {
 
     private  List<String[]> _lines;
 
-
     // Constructor
-    Map_Reader() throws IOException {
-
-
-    }
+    Map_Reader() throws IOException {  }
 
     public GameMap read_Map(String URL, double width, double height) throws IOException {
+        _lines = extract_lines(URL);
         var M = new GameMap(width,height, get_tileset(),get_cell_side(),get_num_of_tiles(),
                             get_tiles_at_row_index(2),get_tiles_at_row_index(3),retrieve_map_without_metadata());
-        _lines = extract_lines(URL);
-
-        create_dictionary_position('P', 4, M.getDictionary_position());
-        create_dictionary_position('T', 5, M.getDictionary_position());
-        M._passable_tiles= M._tiles.stream().filter(b-> b.is_passable).collect(Collectors.toList());
+        fill_dictionary_position('P', 4, M.getDictionary_position());
+        fill_dictionary_position('T', 5, M.getDictionary_position());
         return M;
     }
 
@@ -60,9 +54,6 @@ public class Map_Reader {
         return  Integer.parseInt(_lines.get(1)[2]);
     }
 
-
-
-
     // Return the number of tiles in the rows and columns in the map
     private Pair<Integer, Integer> get_num_of_tiles() {
         return new Pair<>(Integer.parseInt(_lines.get(1)[0]),Integer.parseInt(_lines.get(1)[1]));
@@ -75,23 +66,12 @@ public class Map_Reader {
     private Set<Integer> get_tiles_at_row_index(int index) {
         return  Arrays.stream(_lines.get(index)).parallel().mapToInt(Integer::parseInt).boxed().collect(Collectors.toSet());
     }
-
-    /*
-     Reads the pair of positions of:
-      - the players at row 4th
-      - teleports at row 5th
-     */
-    private Coordinates[] get_positions(int index) {
-        var values = Arrays.stream(_lines.get(index)).parallel().mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
-        return new Coordinates[]{new Coordinates(values.get(0), values.get(1)),new Coordinates(values.get(2), values.get(3))};
-    }
-
-    public void create_dictionary_position(char ID, int index, Map<String, Coordinates> dict){
+    public void fill_dictionary_position(char ID, int index, Map<String, Coordinates> dict){
         var l = Arrays.stream(_lines.get(index)).parallel().mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());;
         //IntStream.range(1,l.size()).mapToObj(i -> new Coordinates(l.get(i-1), l.get(i))).forEach();
         for(int i= 0; i < l.size(); i+=2){
             Coordinates coord = new Coordinates(l.get(i), l.get(i+1));
-            String key = ID+ String.valueOf(i/2);
+            String key = ID + String.valueOf(i/2);
            dict.put(key, coord);
         }
 
