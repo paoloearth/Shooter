dca0a96f20e4f5a2569bdf5395a8de668da08932
matildecastelli <matilde.castelli17@gmail.com>
2021-03-06 19:02:53 +0100

    Made private fields in GameMap

--- src/main/java/units/shooter_developers/GameMap.java
+++ src/main/java/units/shooter_developers/GameMap.java
@@ -14,62 +14,47 @@ import java.util.stream.IntStream;
 public class GameMap {
 
     /* List of tiles that will be used to fast access the blocks property by indices*/
-    List<Tile> _tiles = new ArrayList<>();
+    private List<Tile> _tiles = new ArrayList<>();
 
     /* List of passable blocks */
-    List<Tile> _passable_tiles;
+    private final List<Tile> _passable_tiles;
 
     /* JavaFX component to store and shows the tiles*/
-    Pane _cells = new Pane();
+    private Pane _cells = new Pane();
 
     /* Width & Height of the tiles */
-    private  double _width;
-    private  double _height;
-
-    /* Map_Reader object, delegated to interact with the files */
-    Map_Reader _MR;
-
-    /* URL to the map.csv */
-    String _map_URL;
-    Image _tileset;
-
-    Integer _cell_side;
-    Pair<Integer,Integer> _num_tiles;                              //number of tiles of the map
-    Set<Integer> _set_of_passable;                           //set of passable tiles for the Player
-    Set<Integer> _set_of_NOT_passable_for_projectile;
-    List<String[]> _map;                                           // List of tiles composing the map
-    Map<String,Coordinates> dictionary_position;
+    private final double _width;
+    private final double _height;
+    private final Image _tileset;
+    private final Integer _cell_side;
+    private final Pair<Integer,Integer> _num_tiles;                              //number of tiles of the map
+    private final Set<Integer> _values_of_passable_tiles;                                 //set of passable tiles for the Player
+    private final Set<Integer> _values_of_NOT_passable_tiles_for_projectile;
+    private final List<String[]> _map;                                           // List of tiles composing the map
+    private final Map<String,Coordinates> dictionary_of_positions;
 
     GameMap(double width, double height,
             Image tileset, Integer cell_side,
             Pair<Integer,Integer> num_tiles,Set<Integer> set_of_passable,
             Set<Integer> set_of_NOT_passable_for_projectile, List<String[]> map) throws IOException {
-
-
+        
         /* Set variables  */
-      //  _MR = new Map_Reader(map_URL);
         _width = width;
         _height = height;
-       // _map_URL = map_URL;
-        dictionary_position = new HashMap<>();
+        dictionary_of_positions = new HashMap<>();
         _tileset = tileset;
         _cell_side = cell_side;
         _num_tiles = num_tiles;
-        _set_of_passable = set_of_passable;
-        _set_of_NOT_passable_for_projectile = set_of_NOT_passable_for_projectile;
+        _values_of_passable_tiles = set_of_passable;
+        _values_of_NOT_passable_tiles_for_projectile = set_of_NOT_passable_for_projectile;
        _map =map;
 
         /* */
         this.populateCells();
 
         _passable_tiles = _tiles.stream().filter(b-> b.is_passable).collect(Collectors.toList());
-
-      //  root.getChildren().add(_cells);
-
     }
-
-
-
+    
     public void populateCells() {
 
         int tile_per_row = get_tiles_per_row_of_tileset();
@@ -84,17 +69,14 @@ public class GameMap {
                     pos_row *= _cell_side;
                     pos_col *= _cell_side;
 
-                    boolean passable = _set_of_passable.contains(code);
-                    boolean not_passable_for_p = _set_of_NOT_passable_for_projectile.contains(code);
+                    boolean passable = _values_of_passable_tiles.contains(code);
+                    boolean not_passable_for_p = _values_of_NOT_passable_tiles_for_projectile.contains(code);
 
                     Rectangle2D R = new Rectangle2D(pos_col, pos_row, _cell_side,_cell_side);
 
-                    var myblock = new Tile(i*getTileWidth(), j*getTileHeight(), getTileWidth(), getTileHeight(), passable,not_passable_for_p, _tileset, R);
-
-                    // System.out.println("TILE [ " + i + "," + j + " ] is positioned at [ " +i*getTileWidth() + " , " + j*getTileHeight() + "] ");
-
-
-                    return myblock;
+                    return new Tile(i*getTileWidth(), j*getTileHeight(), getTileWidth(),
+                                            getTileHeight(), passable,not_passable_for_p, _tileset, R);
+                    
                 })
         ).flatMap(s -> s).forEach(_cells.getChildren()::add);
 
@@ -102,14 +84,14 @@ public class GameMap {
 
     }
 
-    public Map<String, Coordinates> getDictionary_position() {
-        return dictionary_position;
+    public Map<String, Coordinates> getDictionary_of_positions() {
+        return dictionary_of_positions;
     }
     // Return the number of tiles in a row of the tileset image
     public int get_tiles_per_row_of_tileset() {
         return (int) (_tileset.getWidth() / _cell_side);
     }
-    Coordinates get_position_of(String id) { return convert_tiles_in_pixel(getDictionary_position().get(id)); }
+    Coordinates get_position_of(String id) { return convert_tiles_in_pixel(getDictionary_of_positions().get(id)); }
 
     //Given the coordinates of a tile
     Coordinates convert_tiles_in_pixel(Coordinates tile_coordinates)
@@ -139,5 +121,8 @@ public class GameMap {
     public Coordinates get_random_location(){
         int index = new Random().nextInt(_passable_tiles.size());
         return _passable_tiles.get(index).get_pixel_of_block_position();
+    }
+    public Pane get_cells() {
+        return _cells;
     }
 }
