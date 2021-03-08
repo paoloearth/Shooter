862d25fc44f35fba88c477a7bc8e3bdbb443d940
jose-dscience <fernandez.fisica@gmail.com>
2021-03-08 12:31:35 +0100

    Removed _width and _height private members from Submenu_component. Removed deprecated methods.

--- src/main/java/units/shooter_developers/MenuAPI/_Submenu_component.java
+++ src/main/java/units/shooter_developers/MenuAPI/_Submenu_component.java
@@ -6,10 +6,6 @@ import javafx.scene.layout.GridPane;
 
 public class _Submenu_component extends GridPane {
 
-
-    private double _width;
-    private double _height;
-
     protected SimpleBooleanProperty all_set = new SimpleBooleanProperty(false);
 
     protected _Submenu_component(double width_scale, double height_scale)
@@ -18,8 +14,8 @@ public class _Submenu_component extends GridPane {
         var height = Menu.getMenuHeight()*height_scale;
 
         /* Functions that deals with the aspects of the menu*/
-        set_height(height);
-        set_width(width);
+        //set_height(height);
+        //set_width(width);
 
         setHgap(10);                                                 // horizontal gap in pixels
         setVgap(10);                                                 // vertical gap in pixels
@@ -29,18 +25,6 @@ public class _Submenu_component extends GridPane {
         this.setPrefSize(width, height);
         this.setMaxSize(width, height);
 
-    }
-
-
-    public void set_width(double _width)   { this._width = _width; }
-    public void set_height(double _height) { this._height = _height; }
-
-    public double get_width() {
-        return _width;
-    }
-
-    public double get_height() {
-        return _height;
     }
 
     public SimpleBooleanProperty get_AllSetProperty() {
