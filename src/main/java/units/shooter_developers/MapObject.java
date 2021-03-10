31f935d8a082e17fcea32aff2d65982a4b506806
paoloearth <paoloearth@gmail.com>
2021-03-10 11:43:30 +0100

    Added static and final to mapobject

--- src/main/java/units/shooter_developers/MapObject.java
+++ src/main/java/units/shooter_developers/MapObject.java
@@ -9,30 +9,30 @@ import javax.annotation.processing.SupportedAnnotationTypes;
 
 public abstract class MapObject extends Pane {
 
-    private  int _width, _height;
-    private  Pair<Double,Double> _scalingFactors;
+    private   int _width, _height;
+    private   Pair<Double,Double> _scalingFactors;
 
     /* Constructors */
     MapObject(Pair<Double,Double> scaling_factors) { set_scalingFactors(scaling_factors); }
     MapObject(int width, int height) { setDimensions(width,height);}
 
-    protected void moveTo(Coordinates coordinates) { relocate(coordinates.getX(),coordinates.getY()); }
+    protected final void moveTo(Coordinates coordinates) { relocate(coordinates.getX(),coordinates.getY()); }
 
-    public double getCurrentXPosition() { return getLayoutX(); }
-    public double getCurrentYPosition() { return getLayoutY(); }
-    public Coordinates getCurrentPosition() {return  new Coordinates(getLayoutX(),getLayoutY());};
+    public final double getCurrentXPosition() { return getLayoutX(); }
+    public final double getCurrentYPosition() { return getLayoutY(); }
+    public final Coordinates getCurrentPosition() {return  new Coordinates(getLayoutX(),getLayoutY());};
 
-    protected void addNodes(Node ... nodes) { getChildren().addAll(nodes); }
+    protected final void addNodes(Node ... nodes) { getChildren().addAll(nodes); }
 
-    public void setDimensions(int width, int height) { set_width(width); set_height(height); }
+    public final void setDimensions(int width, int height) { set_width(width); set_height(height); }
 
     /* Getters */
-    public int get_width() { return _width; }
-    public int get_height() { return _height; }
-    public Pair<Double, Double> getScalingFactors() { return _scalingFactors; }
+    public final int get_width() { return _width; }
+    public final int get_height() { return _height; }
+    public final Pair<Double, Double> getScalingFactors() { return _scalingFactors; }
 
     /* Setters */
-    public void set_width(int width) { _width = width; }
-    public void set_height(int height) { _height = height; }
-    public void set_scalingFactors(Pair<Double, Double> scalingFactors) { _scalingFactors = scalingFactors; }
+    public final void set_width(int width) { _width = width; }
+    public final void set_height(int height) { _height = height; }
+    public final void set_scalingFactors(Pair<Double, Double> scalingFactors) { _scalingFactors = scalingFactors; }
 }
