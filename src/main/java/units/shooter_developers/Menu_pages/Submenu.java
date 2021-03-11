c121302080ece2c8fc99f83d8d78cf7810255aa7
jose-dscience <fernandez.fisica@gmail.com>
2021-03-11 19:54:38 +0100

    Refactoring on submenu

--- src/main/java/units/shooter_developers/Menu_pages/Submenu.java
+++ src/main/java/units/shooter_developers/Menu_pages/Submenu.java
@@ -18,16 +18,11 @@ public class Submenu extends Menu {
         addTextBox("Player_textbox_1", 0, 0, CustomSettings.URL_COMMANDS_P1, 1, CustomSettings.WASD_SCALE, "Who is Player 1?", "Rajoy");
         addTextBox("Player_textbox_2", 0, 1, CustomSettings.URL_COMMANDS_P2, 1, CustomSettings.ARROWS_SCALE, "Who is Player 2?", "Zapatero");
 
-        Map<String, String> Name_URL = new Hashtable<>();
-        Name_URL.put(CustomSettings.ARTIST, CustomSettings.URL_ARTIST);
-        Name_URL.put(CustomSettings.ASTROLOGER, CustomSettings.URL_ASTROLOGER);
-        Name_URL.put(CustomSettings.WARRIOR, CustomSettings.URL_WARRIOR);
+        Map<String, String> Name_URL = generatePlayersUrl();
         addChoiceBox("Player_selection_1", 1, 0, Name_URL, 1,4);
         addChoiceBox("Player_selection_2", 1, 1, Name_URL, 1,4);
 
-        Map<String, String> Map_URL = new Hashtable<>();
-        Map_URL.put(CustomSettings.ISLAND, CustomSettings.URL_MAP_ISLAND_PNG);
-        Map_URL.put(CustomSettings.DESERT, CustomSettings.URL_MAP_DESERT_PNG);
+        Map<String, String> Map_URL = generateMapsUrl();
         addChoiceBox("Map_selection", 2, 0,Map_URL, CustomSettings.MAP_SCALE, 1);
 
         addFreeItem("Play!", 0.6, 0.8);
@@ -41,10 +36,7 @@ public class Submenu extends Menu {
             Players_URL.add(Name_URL.get(getChoiceBoxValue("Player_selection_1")));
             Players_URL.add(Name_URL.get(getChoiceBoxValue("Player_selection_2")));
 
-            Map<String, String> Map_CSV;
-            Map_CSV = new Hashtable<>();
-            Map_CSV.put(CustomSettings.ISLAND, CustomSettings.URL_MAP_ISLAND_CSV);
-            Map_CSV.put(CustomSettings.DESERT, CustomSettings.URL_MAP_DESERT_CSV);
+            Map<String, String> Map_CSV = generateMapDataUrlDictionary();
             ArrayList<String> map_data = new ArrayList<String>();
             map_data.add(Map_CSV.get(getChoiceBoxValue("Map_selection")));
 
@@ -57,5 +49,28 @@ public class Submenu extends Menu {
                 e.printStackTrace();
             }
         });
+    }
+
+    private Map<String, String> generateMapDataUrlDictionary() {
+        Map<String, String> Map_CSV;
+        Map_CSV = new Hashtable<>();
+        Map_CSV.put(CustomSettings.ISLAND, CustomSettings.URL_MAP_ISLAND_CSV);
+        Map_CSV.put(CustomSettings.DESERT, CustomSettings.URL_MAP_DESERT_CSV);
+        return Map_CSV;
+    }
+
+    private Map<String, String> generateMapsUrl() {
+        Map<String, String> Map_URL = new Hashtable<>();
+        Map_URL.put(CustomSettings.ISLAND, CustomSettings.URL_MAP_ISLAND_PNG);
+        Map_URL.put(CustomSettings.DESERT, CustomSettings.URL_MAP_DESERT_PNG);
+        return Map_URL;
+    }
+
+    private Map<String, String> generatePlayersUrl() {
+        Map<String, String> Name_URL = new Hashtable<>();
+        Name_URL.put(CustomSettings.ARTIST, CustomSettings.URL_ARTIST);
+        Name_URL.put(CustomSettings.ASTROLOGER, CustomSettings.URL_ASTROLOGER);
+        Name_URL.put(CustomSettings.WARRIOR, CustomSettings.URL_WARRIOR);
+        return Name_URL;
     }
 }
