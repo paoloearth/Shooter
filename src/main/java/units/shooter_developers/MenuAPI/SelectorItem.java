1bcccdbda52a0cdb801e086da329854885b71395
jose-dscience <fernandez.fisica@gmail.com>
2021-03-15 19:47:19 +0100

    SelectorItem renamed.

--- src/main/java/units/shooter_developers/MenuAPI/SelectorItem.java
+++ src/main/java/units/shooter_developers/MenuAPI/SelectorItem.java
@@ -1,84 +1,84 @@
 package units.shooter_developers.MenuAPI;
-// VISITED
+
+import units.shooter_developers.CustomCheckedException;
+
 import javafx.beans.property.StringPropertyBase;
 import javafx.scene.layout.HBox;
 import javafx.scene.shape.Rectangle;
-import units.shooter_developers.CustomCheckedException;
-
 import java.util.ArrayList;
 
 /************************ SELECTOR ITEM ****************************************/
 
 class SelectorItem extends HBox {
-    private ArrayList<String> _selection_list = null;
-    private int _selection_index;
+    private ArrayList<String> _selectionList;
+    private int _selectionIndex;
     private final String _name;
-    private StringPropertyBase _selection_as_property = null;
+    private StringPropertyBase _selectionAsProperty;
 
-    public SelectorItem(String name, boolean show_name){
-        this(name, 0.25, show_name);
+    public SelectorItem(String name, boolean showName){
+        this(name, 0.25, showName);
     }
 
-    public SelectorItem(String name, double scale_width_text_box, boolean show_name) {
-        _selection_index = 0;
-        _selection_list = new ArrayList<>();
+    public SelectorItem(String name, double scaleWidthTextBox, boolean showName) {
+        _selectionIndex = 0;
+        _selectionList = new ArrayList<>();
         _name = name;
-        _selection_as_property = new StringPropertyBase(){
+        _selectionAsProperty = new StringPropertyBase(){
             @Override
             public Object getBean() { return this; }
             @Override
             public String getName() { return "selectionProperty"; }
         };
-        var selection_section_translation = 0.10;
+        var selectionSectionTranslation = 0.10;
 
-        final NonAnimatedItem name_text_box = new NonAnimatedItem(name);
+        final NonAnimatedItem nameTextBox = new NonAnimatedItem(name);
 
-        final var long_space = new Rectangle(selection_section_translation * Menu.getMenuWidth(), 0);
+        final var longSpace = new Rectangle(selectionSectionTranslation * Menu.getMenuWidth(), 0);
 
-        MenuItem left_arrow_button = new MenuItem("<", 0.04, -1);
-        left_arrow_button.setOnMouseReleased(event -> {
+        MenuItem leftArrowButton = new MenuItem("<", 0.04, -1);
+        leftArrowButton.setOnMouseReleased(event -> {
             previous();
         });
 
-        final var short_space_1 = new Rectangle(0.01 * Menu.getMenuWidth(), 0);
+        final var shortSpace1 = new Rectangle(0.01 * Menu.getMenuWidth(), 0);
 
-        final NonAnimatedItem selection_text_box;
-        selection_text_box = new NonAnimatedItem("not_found", scale_width_text_box, -1);
-        selection_text_box.getNameAsProperty().bind(_selection_as_property);
+        final NonAnimatedItem selectionTextBox;
+        selectionTextBox = new NonAnimatedItem("not_found", scaleWidthTextBox, -1);
+        selectionTextBox.getNameAsProperty().bind(_selectionAsProperty);
 
-        final var short_space_2 = new Rectangle(0.01 * Menu.getMenuWidth(), 0);
+        final var shortSpace2 = new Rectangle(0.01 * Menu.getMenuWidth(), 0);
 
-        final MenuItem right_arrow_button = new MenuItem(">", 0.04, -1);
-        right_arrow_button.setOnMouseReleased(event -> {next();});
+        final MenuItem rightArrowButton = new MenuItem(">", 0.04, -1);
+        rightArrowButton.setOnMouseReleased(event -> {next();});
 
-        if(show_name) {
-            this.getChildren().add(name_text_box);
-            this.getChildren().add(long_space);
+        if(showName) {
+            this.getChildren().add(nameTextBox);
+            this.getChildren().add(longSpace);
         }
-        this.getChildren().add(left_arrow_button);
-        this.getChildren().add(short_space_1);
-        this.getChildren().add(selection_text_box);
-        this.getChildren().add(short_space_2);
-        this.getChildren().add(right_arrow_button);
+        this.getChildren().add(leftArrowButton);
+        this.getChildren().add(shortSpace1);
+        this.getChildren().add(selectionTextBox);
+        this.getChildren().add(shortSpace2);
+        this.getChildren().add(rightArrowButton);
     }
 
     private void next() {
-        _selection_index = _selection_index == _selection_list.size()-1? 0 : _selection_index+1;
-        _selection_as_property.setValue(_selection_list.get(_selection_index));
+        _selectionIndex = _selectionIndex == _selectionList.size()-1? 0 : _selectionIndex +1;
+        _selectionAsProperty.setValue(_selectionList.get(_selectionIndex));
     }
 
     private void previous() {
-        _selection_index = _selection_index == 0? _selection_list.size()-1 : _selection_index-1;
-        _selection_as_property.setValue(_selection_list.get(_selection_index));
+        _selectionIndex = _selectionIndex == 0? _selectionList.size()-1 : _selectionIndex -1;
+        _selectionAsProperty.setValue(_selectionList.get(_selectionIndex));
     }
 
     protected void addTag(String selection_tag) {
-        _selection_list.add(selection_tag);
-        _selection_as_property.setValue(_selection_list.get(_selection_index));
+        _selectionList.add(selection_tag);
+        _selectionAsProperty.setValue(_selectionList.get(_selectionIndex));
     }
 
     protected String getText() {
-        return _selection_as_property.getValue();
+        return _selectionAsProperty.getValue();
     }
 
     protected String getName() {
@@ -86,16 +86,16 @@ class SelectorItem extends HBox {
     }
 
     protected StringPropertyBase getSelectionAsProperty(){
-        return _selection_as_property;
+        return _selectionAsProperty;
     }
 
     protected void setDefaultIndex(int index) throws CustomCheckedException.IndexOutOfRange {
-        if(index >= _selection_list.size()){throw new CustomCheckedException.IndexOutOfRange(
+        if(index >= _selectionList.size()){throw new CustomCheckedException.IndexOutOfRange(
                 "Menu selector item",
-                _selection_list.size()-1,
+                _selectionList.size()-1,
                 index, SelectorItem.class);}
 
-        _selection_index = index;
-        _selection_as_property.setValue(_selection_list.get(_selection_index));
+        _selectionIndex = index;
+        _selectionAsProperty.setValue(_selectionList.get(_selectionIndex));
     }
 }
