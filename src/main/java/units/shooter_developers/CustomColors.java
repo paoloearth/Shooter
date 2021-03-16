11f54821a961f2279fdb48edef2bf215c278927c
matildecastelli <matilde.castelli17@gmail.com>
2021-03-16 17:43:02 +0100

    Changed color menu

--- src/main/java/units/shooter_developers/CustomColors.java
+++ src/main/java/units/shooter_developers/CustomColors.java
@@ -20,11 +20,11 @@ public class CustomColors {
     public static final Color CLICKED_BACKGROUND_COLOR_DARK = Color.DARKVIOLET;
     public static final Color DEAD_COLOR_DARK = Color.DARKGREY;
 
-    public static final Color BASIC_PRIMARY_COLOR_LIGHT = Color.WHEAT;
+    public static final Color BASIC_PRIMARY_COLOR_LIGHT = Color.WHITE;
     public static final Color SELECTED_PRIMARY_COLOR_LIGHT = Color.DARKRED;
     public static final Color BASIC_SECONDARY_COLOR_LIGHT = Color.BLACK;
     public static final Color SELECTED_SECONDARY_COLOR_LIGHT = Color.WHITESMOKE;
     public static final Color CLICKED_BACKGROUND_COLOR_LIGHT = Color.ORANGERED;
-    public static final Color DEAD_COLOR_LIGHT = Color.SANDYBROWN;
+    public static final Color DEAD_COLOR_LIGHT = Color.WHITE;
 
 }
